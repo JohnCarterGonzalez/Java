818e61380184ed5632160d18e467f113de67f50d
JohnCarter <102844682+JohnCarterGonzalez@users.noreply.github.com>
2022-06-03 10:55:32 -0700

    routing work

--- SpringProjects/HelloWorld/src/main/java/com/johncartergonzalez/HelloWorld/HelloWorldApplication.java
+++ SpringProjects/HelloWorld/src/main/java/com/johncartergonzalez/HelloWorld/HelloWorldApplication.java
@@ -3,11 +3,26 @@ package com.johncartergonzalez.HelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World!";
+	}
+
+	@RequestMapping("/test")
+	public String test() {
+		return "Be ready for the test!";
 	}
 
 }
