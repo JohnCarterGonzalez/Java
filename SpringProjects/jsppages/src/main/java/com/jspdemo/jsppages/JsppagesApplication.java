044ca6695e011cbad828b84af049b081b6ffba0e
JohnCarter <102844682+JohnCarterGonzalez@users.noreply.github.com>
2022-06-06 11:39:59 -0700

    Java Fundamentals

---
+++ SpringProjects/jsppages/src/main/java/com/jspdemo/jsppages/JsppagesApplication.java
@@ -0,0 +1,13 @@
+package com.jspdemo.jsppages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsppagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsppagesApplication.class, args);
+	}
+
+}
