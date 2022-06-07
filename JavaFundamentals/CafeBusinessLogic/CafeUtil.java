7621f464f6ea84d22ca505d2aac8153b6d0b698b
JohnCarter <102844682+JohnCarterGonzalez@users.noreply.github.com>
2022-06-06 19:26:42 -0700

    CafeJava worked on basic logic in Java

---
+++ JavaFundamentals/CafeBusinessLogic/CafeUtil.java
@@ -0,0 +1,49 @@
+import java.util. ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CafeUtil {
+    //reward system for customers. calculats how many drinks they need after 10 weeks
+    // write a method that sums together ever consecutive integer from 1 to 10 and returns the sum
+    public int getStreakGoal(int numWeeks) {
+        int sum = 0;
+        for (int i = 1; i <= numWeeks; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+ 
+    //given an array of item prices from an order, sum all of the prices in the array and return the total
+    public double getOrderTotal(double[] prices) {
+        double total = 0;
+        for (int i = 0; i < prices.length; i++) {
+            total += prices[i];
+        }
+        return total;
+    }
+
+    //given an array of menu items, print each item on a separate line
+    public void displayMenu(ArrayList<String> menuItems) {
+        int index = 0;
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println((index++) + ": " + menuItems.get(i));
+        }
+    }
+
+    //print a string to the console
+    // get the user input from the terminal and store it in a variable userName
+    //print the userName to the console saying, "Hello, [user name here]"
+    //next print "there are ___ people in front of you" using the nymber for how many people are ahead of them (how many items already in the array)
+    //add the customers name to the given list and print the list 
+    //no need to return anything 
+    public void addCustomer(ArrayList<String> customers) {
+        System.out.println("Please enter your name: ");
+        String userName = System.console().readLine();
+        
+        System.out.println("Hello, " + userName);
+        System.out.println("There are " + customers.size() + " people in front of you.");
+        customers.add(userName);
+        System.out.println(customers);
+    }
+
+}
---
+++ JavaFundamentals/CafeBusinessLogic/README.md
@@ -0,0 +1,8 @@
+Objectives:
+
+    Write methods to solve basic logic problems in Java.
+    Practice Array and ArrayList.
+    Practice loops.
+    Understand the difference between printing and returning a value from a method.
+    Create your own methods file
+    Write a test file, and create an instance of another class to test the functionality.
