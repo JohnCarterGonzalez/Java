61ff0922b44fc84ba48c90b356ddfb77aa8da87e
JohnCarter <102844682+JohnCarterGonzalez@users.noreply.github.com>
2022-06-09 17:11:41 -0700

    worked on algos for Java

--- WeeklyExercises/exercises.java
+++ WeeklyExercises/exercises.java
@@ -9,24 +9,30 @@ class Result {
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
 
+    //Given five positive integers, find the minimum and maximum values that can be calculated 
+    //by summing exactly four of the five integers. 
+    //Then print the respective minimum and maximum values as a single line of two space-separated long integers.
     public static void miniMaxSum(List<Integer> arr) {
         // Write your code here
-        int min = 0;
-        int max = 0;
-        int sum = 0;
+        long min = 0;
+        long max = 0;
+        long sum = 0;
         for (int i = 0; i < arr.size(); i++) {
             sum += arr.get(i);
         }
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i) != arr.get(arr.size() - 1)) {
-                min += arr.get(i);
+        min = sum - arr.get(0);
+        max = sum - arr.get(0);
+        for (int i = 1; i < arr.size(); i++) {
+            long temp = sum - arr.get(i);
+            if (temp < min) {
+                min = temp;
             }
-        }
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i) != arr.get(0)) {
-                max += arr.get(i);
+            if (temp > max) {
+                max = temp;
             }
         }
         System.out.println(min + " " + max);
     }
+
+
 }
