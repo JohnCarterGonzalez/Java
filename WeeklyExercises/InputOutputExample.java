c7bd087be938cabb2191f764503ab3ae91bca4c6
JohnCarter <102844682+JohnCarterGonzalez@users.noreply.github.com>
2022-06-10 08:27:04 -0700

    Working

--- WeeklyExercises/InputOutputExample.java
+++ WeeklyExercises/InputOutputExample.java
@@ -3,15 +3,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class InputOutputExample {
-    public static void main(String[] args) {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      System.out.print("Enter String: ");
-      String s = br.readLine();
-      System.out.print("Enter Integer: ");
-      try {
-        int i = Integer.parseInt(br.readLine());
-      } catch(NumberFormatException nfe){
-        System.err.print("Invalid Format");
-      }
-    }
+	public static void main(String[] args) {
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	  System.out.print("Enter String: ");
+	  String s = br.readLine();
+	  System.out.print("Enter Integer: ");
+	  try {
+		int i = Integer.parseInt(br.readLine());
+	  } catch(NumberFormatException nfe){
+		System.err.print("Invalid Format");
+	  }
+	}
 }
