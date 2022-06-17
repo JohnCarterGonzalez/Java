a6cacf80c47e1709fe2352d775c17aaf368fbbb3
JohnCarter <102844682+JohnCarterGonzalez@users.noreply.github.com>
2022-06-17 16:48:41 -0700

    ZooKeeper
    working on inheritance and packages

---
+++ JavaProjects/ZooKeeperTwo/src/main/java/GiantBat.java
@@ -0,0 +1,20 @@
+import Mammal.Mammal;
+
+public class GiantBat {
+    Mammal mammal = new Mammal();
+
+    public void fly() {
+        mammal.energyLevel -= 50;
+        System.out.println("Flllrrrttt Flllrrtt");
+    }
+
+    public void eatHumans(){
+        mammal.energyLevel += 25;
+    }
+
+    public void attackTown(){
+        mammal.energyLevel -= 100;
+        System.out.println("*Sound of a town on fire*");
+    }
+
+}
