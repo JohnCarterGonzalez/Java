5848e569494b2f1f4db9b8c43e84a89ae6d6bd89
JohnCarter <102844682+JohnCarterGonzalez@users.noreply.github.com>
2022-06-17 16:50:47 -0700

    Delete JavaProjects/ZooKeeper directory

--- JavaProjects/ZooKeeper/src/main/java/Mammal.java
+++
@@ -1,8 +0,0 @@
-public class Mammal {
-    int energyLevel = 300;
-
-    //display the energy level for each mammal
-    public void displayEnergy() {
-        System.out.println("The current energy level is: " + energyLevel);
-    }
-}
